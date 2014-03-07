3212520638c68e32f958e31011e64475acc9dfb5
unknown <talmo.christian@gmail.com>
2014-03-07 03:29:29 +0200

    fixes

--- src/com/heartpirates/Emoticon.java
+++ src/com/heartpirates/Emoticon.java
@@ -21,15 +21,31 @@ public class Emoticon {
 	int h = 20;
 	BufferedImage bimg;
 
+	boolean headless = GraphicsEnvironment.isHeadless();
+
 	static Color fgColor = new Color(0x202020);
 	static Color bgColor = new Color(0xCFBFAD);
 
-	public static final Font FONT = getFont("res/font/arialuni.ttf");
+	public static final Font FONT1 = getFont("fonts/arialuni.ttf", 14f);
+	public static final Font FONT2 = getFont("fonts/hkgokukaikk.ttf", 16f);
+	public static final Font FONT3 = new Font("Unicode", Font.PLAIN, 12);
+	public static Font FONT = FONT1;
 
 	public static String[] emotes = { "(　ﾟДﾟ)＜!!", "(¬д¬。)", "(＃｀д´)ﾉ",
-			"(/ﾟДﾟ)/", "（；¬＿¬)", "(」゜ロ゜)」", "(；￣Д￣）", "＼(｀0´)／", "-",
+			"(/ﾟДﾟ)/", "（；¬＿¬)", "(」゜ロ゜)」", "(；￣Д￣）", "＼(｀0´)／",
 			"(╯°□°）╯︵ ┻━┻", "(╯°Д°）╯︵/(.□ . \\)", "ヽ(#ﾟДﾟ)ﾉ┌┛Σ(ノ´Д`)ノ",
-			"d(｀⌒´メ)z", "(∩｀-´)⊃━☆ﾟ.*･｡ﾟ", "ヽ₍⁽ˆ⁰ˆ⁾₎」♪♬" };
+			"d(｀⌒´メ)z", "(∩｀-´)⊃━☆ﾟ.*･｡ﾟ", "ヽ₍⁽ˆ⁰ˆ⁾₎」♪♬", "ヽ(ﾟДﾟ)ﾉ", "∑(;°Д°)",
+			"ヽ（゜ロ゜；）ノ", "(⊙︿⊙✿)", "(⊙△⊙✿)", "ヾ(´･ ･｀｡)ノ”", "⊙﹏⊙", "(♠•﹏•)",
+			"〆(・∀・＠)", "φ(^ω^*)ﾉ", "φ(°ρ°*)メ", "＿〆(。。)", "〆(´Ｕ_｀*)",
+			"(,, ･∀･)ﾉ゛", "(。･д･)ﾉﾞ", "(｡･ω･)ﾉﾞ", "ヽ(´･ω･`)､", "(=ﾟωﾟ)ノ",
+			"( ﾟ▽ﾟ)/", "(。-ω-)ﾉ", "ヾ( ‘ – ‘*)", "ヾ(･ω･`｡)", "ヾ(・ω・ｏ)",
+			"ヽ(๏∀๏ )ﾉ", "(´⊙ω⊙`)！", "(((( ;°Д°))))", "(*ﾟﾛﾟ)", "щ(゜ロ゜щ)",
+			"o(´^｀)o", "(_ _)ヾ(‘ロ‘)", "＿ﾉ乙(､ﾝ､)_", "_(┐「ε:)_", "_(：3 」∠ )_",
+			"（/｡＼)", "（*/∇＼*）", "(*ﾉ▽ﾉ)", "(*ﾉωﾉ)", "(/ω＼)", "(つω⊂* )",
+			"(*ﾟｰﾟ)ゞ", "ε=ε=(っ*´□`)っ", "ヽ(￣д￣;)ノ=3=3=3", "( /)u(\\ )",
+			"ヽ(●ﾟ´Д｀ﾟ●)ﾉﾟ", "(ノД`)・゜・。", "⊂(‘ω’⊂%20)))Σ≡=─~~", "ε===(っ≧ω≦)っ",
+			"ε=ε=ε=ε=ε=ε=┌(;ʘ∀ʘ)┘", "┗( ●-﹏ ｀｡)づ", "⌒(｡･.･｡)⌒", "(•ㅅ•)",
+			"( ◐ω◐ )", "─=≡Σ((( つ•̀ω•́)つ", "ε=ε=(っ*´□`)っ" };
 
 	public static String getEmote(int n) {
 		if (n < 0)
@@ -37,14 +53,20 @@ public class Emoticon {
 		return emotes[n % emotes.length];
 	}
 
-	public static Font getFont(String path) {
+	public static Font getFont(String path, float size) {
+		Font f = null;
 		try {
-			Font f = Font.createFont(Font.TRUETYPE_FONT, Emoticon.class
-					.getClassLoader().getResourceAsStream(path));
-
-			return f.deriveFont(14f);
+			try {
+				f = Font.createFont(Font.TRUETYPE_FONT, Emoticon.class
+						.getClassLoader().getResourceAsStream(path));
+			} catch (Exception e) {
+				if (f == null) {
+					f = Font.createFont(Font.TRUETYPE_FONT, Emoticon.class
+							.getClassLoader().getResourceAsStream("font.ttf"));
+				}
+			}
+			return f.deriveFont(size);
 		} catch (Exception e) {
-
 		}
 		return null;
 	}
@@ -53,13 +75,21 @@ public class Emoticon {
 		if (emote.length() < 1)
 			emote = ("シ (╯°□°）╯︵ ┻━┻ シ");
 
-		w = emote.length() * 12 + 1;
+		w = emote.length() * 16 + 1;
+
 		bimg = new BufferedImage(w, h, BufferedImage.TYPE_INT_ARGB);
 		Graphics g = bimg.getGraphics();
-		if (FONT != null)
+		if (FONT != null) {
 			g.setFont(FONT);
+		} else {
+			g.setFont(FONT1);
+		}
+
 		Font f = g.getFont();
 		int size = f.getSize();
+
+		if (!headless)
+			System.out.println(f.getName());
 
 		g.setColor(fgColor);
 		g.drawString(emote, 0, size);
@@ -130,7 +160,13 @@ public class Emoticon {
 
 	public static void main(String[] args) {
 		if (args.length > 0) {
-			String s = args[0];
+			String[] vars = args[0].split("\\?");
+			String s = vars[0];
+
+			if (s.equalsIgnoreCase("jp")) {
+				FONT = FONT2;
+			}
+
 			if (s.equalsIgnoreCase("test")) {
 				System.out.println("Test");
 			} else if (s.equalsIgnoreCase("print")) {
@@ -142,7 +178,7 @@ public class Emoticon {
 				}
 			} else {
 				try {
-					String emote = URLDecoder.decode(args[args.length - 1],
+					String emote = URLDecoder.decode(vars[vars.length - 1],
 							"UTF-8");
 					Emoticon e = new Emoticon(emote);
 					// write the bytes to the output stream
@@ -152,7 +188,7 @@ public class Emoticon {
 					e1.printStackTrace();
 				}
 			}
-		} else {
+		} else { // no args
 			try {
 				if (GraphicsEnvironment.isHeadless()) {
 					Emoticon e = new Emoticon(getEmote(-1)); // random emote
@@ -174,7 +210,8 @@ public class Emoticon {
 		JFrame frame = new JFrame("Emoticon");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		Emoticon e = new Emoticon(getEmote(-1));
+		String emote = getEmote(-1);
+		Emoticon e = new Emoticon(emote);
 
 		ByteArrayInputStream input = new ByteArrayInputStream(e.getPNG());
 		BufferedImage bimg = ImageIO.read(input);
